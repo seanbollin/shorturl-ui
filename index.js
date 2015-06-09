3259d45ecebf8d948d98705e25cc43e9cad8455c
seanbo <sbollin@societyconsulting.com>
2015-06-08 19:55:21 -0700

    change port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/shorturl', function (req, res) {
 	});
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
